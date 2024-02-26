bec41324578d0f7670c8cc2c523a3ed335fe08f3
Josue Ortiz <dortizo@unemi.edu.ec>
2024-02-26 12:06:30 -0500

    ORM aprendiendo

--- core/erp/models.py
+++ core/erp/models.py
@@ -2,7 +2,34 @@ from django.db import models
 from datetime import datetime
 
 # Create your models here.
+class Tipo(models.Model):
+    nombre = models.CharField(max_length=150, verbose_name='Nombre', unique=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Tipo'
+        verbose_name_plural = 'Tipos'
+        db_table = 'tipo'
+        ordering = ['id']
+
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name='Nombre')
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        db_table = 'categoria'
+        ordering = ['id']
+
 class Empleado(models.Model):
+    categoria = models.ManyToManyField(Categoria)
+    tipo = models.ForeignKey(Tipo, on_delete=models.CASCADE)
     nombres = models.CharField(max_length=150, verbose_name='nombres')
     cedula = models.CharField(max_length=10, verbose_name='cedula', unique=True)
     fecha_registro = models.DateField(default=datetime.now, verbose_name='Fecha de registro')
@@ -15,12 +42,12 @@ class Empleado(models.Model):
     avatar = models.ImageField(upload_to='avatar/%Y/%m/%d', null=True, blank=True)
     curriculum = models.FileField(upload_to='curriculum/%Y/%m/%d', null=True, blank=True)
 
-def __str__(self):
-    return self.nombres
+    def __str__(self):
+        return self.nombres
 
-class Meta:
-    verbose_name = 'Empleado'
-    verbose_name_plural = 'Empleados'
-    db_table = 'empleado'
-    ordering = ['id']
+    class Meta:
+        verbose_name = 'Empleado'
+        verbose_name_plural = 'Empleados'
+        db_table = 'empleado'
+        ordering = ['id']
 
--- core/erp/tests.py
+++ core/erp/tests.py
@@ -1,3 +1,32 @@
+from djangoProject.wsgi import *
 from django.test import TestCase
+from core.erp.models import Tipo
 
-# Create your tests here.
+#LISTAR
+
+#Select * from table
+#consulta = Tipo.objects.all()
+#print(consulta)
+
+#insert
+#t = Tipo(nombre='Prueba2')
+#t.nombre = 'Prueba'
+#t.save()
+
+#edicion
+#t2 = Tipo.objects.get(id=2)
+#t2.nombre = "Programador"
+#t2.save()
+
+#Eliminación
+
+#t = Tipo.objects.get(id=4)
+#t.delete()
+
+#Filtrar una lista
+#obj = Tipo.objects.filter(nombre__icontains='pr')
+#obj = Tipo.objects.filter(nombre__endswith='r')
+#obj = Tipo.objects.filter(nombre__endswith='r').query
+obj = Tipo.objects.filter(nombre__endswith='r').exclude(id=2)
+
+print(obj)
