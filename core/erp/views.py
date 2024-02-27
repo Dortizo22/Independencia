8181cc3a7e76d863241812c891c23cc4467248d6
Josue Ortiz <dortizo@unemi.edu.ec>
2024-02-27 09:11:14 -0500

    Practica de URLS y Vistas

--- core/erp/views.py
+++ core/erp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
+def myfirstview(request):
+    data = {
+        'message': 'Hello',
+        'name': 'Josue Ortiz'
+    }
+    return JsonResponse(data)
+
+def indexview(request):
+    return HttpResponse('Esta es la Página Principal')
--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.erp.views import myfirstview, indexview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', indexview),
+    path('prueba/', myfirstview),
 ]
