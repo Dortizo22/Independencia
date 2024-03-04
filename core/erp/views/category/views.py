1bb0b3afe848f906bb554b3246190fd46ca6e1a9
Josue Ortiz <dortizo@unemi.edu.ec>
2024-03-04 11:45:34 -0500

    Vistas basadas en clases - ListVie

--- core/erp/views/category/views.py
+++ core/erp/views/category/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from lib2to3.fixes.fix_input import context
 
+from django.shortcuts import render
+from django.views.generic import ListView
 from core.erp.models import Category
 
 
@@ -9,3 +11,12 @@ def category_list(request):
         'categories': Category.objects.all()
     }
     return render(request, 'category/list.html', data)
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Listado de Categorias'
+        return context
