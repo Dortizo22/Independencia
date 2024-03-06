c7508a6f97c0b427c6d0088ab3997665cd57f16c
Josue Ortiz <dortizo@unemi.edu.ec>
2024-03-06 08:41:06 -0500

    ListView e implementación de datatables

--- core/erp/tests.py
+++ core/erp/tests.py
@@ -1,32 +1,11 @@
 from djangoProject.wsgi import *
-from django.test import TestCase
-from core.erp.models import Tipo
+from core.erp.models import *
 
-#LISTAR
+data = ['leche y derivados', 'Carnes, pescado y huevos', 'Patatas, legumbres y frutos secos',
+        'verduras y hortalizas', 'frutas', 'Cereales y derivados', 'azucar y dulces',
+        'Grasas, aceite y mantequilla']
 
-#Select * from table
-#consulta = Tipo.objects.all()
-#print(consulta)
-
-#insert
-#t = Tipo(nombre='Prueba2')
-#t.nombre = 'Prueba'
-#t.save()
-
-#edicion
-#t2 = Tipo.objects.get(id=2)
-#t2.nombre = "Programador"
-#t2.save()
-
-#Eliminación
-
-#t = Tipo.objects.get(id=4)
-#t.delete()
-
-#Filtrar una lista
-#obj = Tipo.objects.filter(nombre__icontains='pr')
-#obj = Tipo.objects.filter(nombre__endswith='r')
-#obj = Tipo.objects.filter(nombre__endswith='r').query
-obj = Tipo.objects.filter(nombre__endswith='r').exclude(id=2)
-
-print(obj)
+for i in data:
+    cat = Category(name=i)
+    cat.save()
+    print('Guardado registro N°{}'.format(cat.id))
