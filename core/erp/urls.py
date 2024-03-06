28accc0464ea9d905e247f9a4daefb6a1336346a
Josue Ortiz <dortizo@unemi.edu.ec>
2024-03-06 10:53:57 -0500

    sobreescritura del metodo dispatch y la implementación de decoradores

--- core/erp/urls.py
+++ core/erp/urls.py
@@ -4,5 +4,6 @@ from core.erp.views.category.views import *
 app_name = 'erp'
 
 urlpatterns = [
-    path('category/list/', CategoryListView.as_view(), name='category-list')
+    path('category/list/', CategoryListView.as_view(), name='category-list'),
+    path('category/list2/', category_list, name='category-list2')
 ]
--- core/erp/views/category/views.py
+++ core/erp/views/category/views.py
@@ -1,7 +1,9 @@
 from lib2to3.fixes.fix_input import context
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 from core.erp.models import Category
 
 
@@ -16,6 +18,9 @@ class CategoryListView(ListView):
     model = Category
     template_name = 'category/list.html'
 
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Listado de Categorias'
